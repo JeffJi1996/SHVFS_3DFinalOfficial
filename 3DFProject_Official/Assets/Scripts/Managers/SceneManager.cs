184391708366f41db07bc45c75f0b2813e6cd069
GDLAB-MSI09\GD Student <zhangyijie16@163.com>
2021-07-14 11:13:32 +0800

    TitleScene

---
+++ 3DFProject_Official/Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : Singleton<SceneManager>
+{
+   protected override void Awake()
+   {
+      base.Awake();
+      DontDestroyOnLoad(this);
+   }
+
+   public void NextScene()
+   {
+      UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+   }
+}
