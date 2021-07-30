2fa3948309e0ecb8bf42cd02af84f1c25ed62535
GDLAB-MSI05\GD Student <jjc1114@sina.com>
2021-07-30 17:35:52 +0800

    蛛网+柜子

---
+++ 3DFProject_Official/Assets/Scripts/Items/Cabinet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cabinet : MonoBehaviour
+{
+    private bool isDestroyed;
+    [SerializeField]private GameObject LeftMesh;
+    [SerializeField]private GameObject RightMesh;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<PlayerMovement>()!= null&&!isDestroyed)
+        {
+            PlayerMovement.Instance.AdjustWalkingSpeed(CabinetManager.Instance.GetSpeedRate());
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.GetComponent<PlayerMovement>() != null && !isDestroyed)
+        {
+            PlayerMovement.Instance.AdjustWalkingSpeed(1/CabinetManager.Instance.GetSpeedRate());
+        }
+    }
+
+    void Initialize()
+    {
+        isDestroyed = false;
+        LeftMesh.SetActive(true);
+        RightMesh.SetActive(true);
+    }
+}
